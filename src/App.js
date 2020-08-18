df25a2839a8b78821397e6c35b0fb256f866b2b8
Colin Ritchey <colin.ritchey@totalbrain.com>
2020-08-18 10:51:01 -0700

    testing onloadmetadta

--- src/App.js
+++ src/App.js
@@ -5,30 +5,51 @@ import './App.css';
 // import Music from './Music';
 // import MusicWithHooks from './MusicWithHooks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Music 
-          src="https://music-cdn.icons8.com/preview_low/950/fa97a7c8-0077-48e1-81a9-33039cd4038a.mp3"
-        />
-        <div style={{ width: '30%' }}>
-          <InputRange initialValue={25} />
-        </div> */}
+class App extends React.Component {
+  componentDidMount() {
+    const video = document.getElementById('audio');
+  
+    video.preload = 'metadata';
 
-        <button
-          onClick={() => {
-            const video = document.getElementById('audio');
-            video.volume = 0.2;
-            video.play();
-          }}
-        >
-          play
-        </button>
+    video.onloadedmetadata = function() {
+      window.URL.revokeObjectURL(video.src);
+      var duration = video.duration;
 
-      </header>
-    </div>
-  );
+      console.log('duration in on load metadata: ', duration)
+      // debugger
+      // myVideos[myVideos.length - 1].duration = duration;
+      // updateInfos();
+    }
+
+    console.log('duration: ', video.duration);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <Music 
+            src="https://music-cdn.icons8.com/preview_low/950/fa97a7c8-0077-48e1-81a9-33039cd4038a.mp3"
+          />
+          <div style={{ width: '30%' }}>
+            <InputRange initialValue={25} />
+          </div> */}
+  
+          <button
+            onClick={() => {
+              const video = document.getElementById('audio');
+  
+              video.volume = 0.2;
+              video.play();
+            }}
+          >
+            play
+          </button>
+  
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
